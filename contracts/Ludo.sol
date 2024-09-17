c2a0538b8b42ff2e4cafc13262c4a4c0ef9b8c0e
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-09-17 18:05:00 +0100

    add dice function

--- contracts/Ludo.sol
+++ contracts/Ludo.sol
@@ -3,16 +3,16 @@
 pragma solidity ^0.8.24;
 
 import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Ludo  {
+contract Ludo is VRFConsumerBase {
 
     address owner;
 
-    array diceValues = [1, 2, 3, 4, 5, 6];
+    array dicePossibleValues = [1, 2, 3, 4, 5, 6];
 
     constructor() {
         owner = msg.sender
